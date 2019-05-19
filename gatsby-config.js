9307aca8f6e6a3f9301676966c9fcca4592ed769
ttaniguchi <ttaniguchi@oikaze.jp>
2019-05-19 23:49:36 +0900

    [add] gatsby-plugin-google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,12 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
